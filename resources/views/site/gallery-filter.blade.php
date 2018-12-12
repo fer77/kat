850172f7e8811aaef1a55adb3af652a894dc2de6
Fernando Fernandez <fernando.fernandez@chghealthcare.com>
2018-12-11 21:14:03 -0700

    feature: added filter for earings

--- resources/views/site/gallery-filter.blade.php
+++ resources/views/site/gallery-filter.blade.php
@@ -1,7 +1,7 @@
 <ul class="category-filter simplefilter mb-4">
     <li class="active" data-filter="all">All</li>
-    <li data-filter="1">Wall hangings</li>
-    <li data-filter="2">Earings</li>
+    <li data-filter="68892008">Wall hangings</li>
+    <li data-filter="69151501">Earings</li>
     <li data-filter="3">Table runners</li>
     <li data-filter="4">Custom designs</li>
 </ul>
