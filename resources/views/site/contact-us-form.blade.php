fcb3eb46f639c49d6b8af10e8ad39652161f6b2e
Fernando <ffmp777@yahoo.com>
2018-12-13 18:59:42 -0700

    removed hard coded text from contact form.

--- resources/views/site/contact-us-form.blade.php
+++ resources/views/site/contact-us-form.blade.php
@@ -2,16 +2,16 @@
 <form class="contact-form mt-4">
     <div class="row">
         <div class="col-md-5">
-            <input type="text" class="form-control-custom mb-4" placeholder="Name" value="Your name">
+            <input type="text" class="form-control-custom mb-4" placeholder="Name" value="">
         </div>
         <div class="col-md-5">
-            <input type="text" class="form-control-custom mb-4" placeholder="Email address" value="Email address">
+            <input type="text" class="form-control-custom mb-4" placeholder="Email address" value="">
         </div>
         <br />
     </div>
     <div class="row">
         <div class="col-md-10">
-            <textarea class="form-control-custom mb-4" rows="3">Your Message</textarea><br />
+            <textarea class="form-control-custom mb-4" rows="3" placeholder="Your Message"></textarea><br />
             <button type="submit" class="btn btn-primary btn-md pill-btn mb-4">Send Message</button>
         </div> 
     </div>
