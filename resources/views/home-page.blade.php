724833c5aa92d949d05acc9af5abbc04f5edcee3
Fernando <fernando.fernandez@chghealthcare.com>
2019-04-08 16:35:26 -0600

    resolved merge conflicts

--- resources/views/home-page.blade.php
+++ resources/views/home-page.blade.php
@@ -4,15 +4,15 @@
 	@include("site.nav")
 	@include("site.hero")
 </section>
-		
+
 <section id="portfolio" class="gallery text-center">
 	<div class="container-fluid">
 		<a name="gallery"></a>
 		<h2 class="mb-4">Our Work</h2>
-		
+
 		<!--Gallery Filter-->
 		@include("site.gallery-filter")
-	
+
 		<!--Gallery-->
 		@include("site.gallery")
 	</div>
@@ -20,5 +20,5 @@
 
 <section class="cta text-center">
 	<!--CTA-->
-	@include("site.call-to-action")		
+	@include("site.call-to-action")
 </section>
