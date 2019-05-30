165edd01ae326f059434a70898b9afc87f662887
Fernando <fernando.fernandez@chghealthcare.com>
2019-05-30 16:42:48 -0600

    feature(admin area): created a new layout for login/register forms.

---
+++ resources/views/layouts/site.blade.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+</head>
+
+<body>
+    @if (Route::has('register'))
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                @yield('content')
+            </div>
+        </div>
+    </div>
+    @endif
+    @if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
+    <!-- Scripts -->
+    <script src="{{ mix('/js/app.js') }}"></script>
+</body>
+
+</html>
